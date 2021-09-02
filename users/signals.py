2057afc9b76476d18b85fc59b0f9831864ef3da8
mhdshjn <mohammedshajin1997@gmail.com>
2021-09-02 14:21:35 +0530

    signal added

---
+++ users/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save, post_delete
+
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Profile
+
+def createProfile(sender, instance, created, **kwargs):
+    print("profile signal trigger")
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        )
+
+def deleteUser(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender= Profile)
